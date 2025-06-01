468e603154221799f47f8cc2c0c7879b12b650f5
= <=>
2025-06-01 12:09:10 +0700

    update materi in folder linked list

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
-def stack():
-    s=[]
-    return s
-def push(s,data):
-    s.append(data)
-def pop(s):
-    data=s.pop()
-    return(data)
-def peek(s):
-    return(s[len(s)-1])
-def isEmpty(s):
-    return (s==[])
-def size(s):
-    return(len(s))
+index = 1
+temp = []
+genap = 0
+ganjil = 0
+total = 0
+user = int(input("Berapa angka yang ingin dimasukkan? "))
+while index < user+1:
+    user2 = int(input(f"Masukkan angka ke-{index} : "))
+    temp.append(user2)
+    index += 1
 
-ekspresi = input("Masukkan ekspresi matematika: ")
-st = stack()
-seimbang = True
-pasangan = {')': '(', ']': '[', '}': '{'}
-
-for char in ekspresi:
-    if char in "([{":
-        push(st, char)
-    elif char in ")]}":
-        if isEmpty(st) or peek(st) != pasangan[char]:
-            seimbang = False
-            break
-        else:
-            pop(st)
-
-if not isEmpty(st):
-    seimbang = False
-
-if seimbang:
-    print("Kurung seimbang")
-else:
-    print("Kurung tidak seimbang")
-
+iterasi = 0
+print("\nAngka-angka yang kamu masukkan:")
+for i in temp:
+    if i % 2 == 0:
+        genap += 1
+    else:
+        ganjil += 1
+    angka = i
+    iterasi += 1
+    total += i
+    print(f"{iterasi}. {angka}")
+rata_rata = total / user
+print(f"\nTotal semua angka: {total}")
+print(f"Rata-rata angka: {rata_rata}")
+print(f"Jumlah angka genap: {genap}")
+print(f"Jumlah angka ganjil: {ganjil}")
